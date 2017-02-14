58582734899420369f187f2d6125132c3f1e8937
Jack Klika <jackklika@users.noreply.github.com>
2017-02-14 12:36:36 -0600

    Update ATM.java

--- src/ATM.java
+++ src/ATM.java
@@ -4,21 +4,24 @@ import java.util.*;
 public class ATM {
 
 	// User's account number
-	int accountNum = 0;
+	private static int accountNum = 0;
 	
 	// User's pin
-	int pin = 0;
+	private static int pin = 0;
 	
+	private static Account userAccount;
 
+	private static Bank myBank = new Bank();
+	
 	
 	// Validation Exceptions
 	
-	Bank myBank = new Bank();
+	
 	
 	// Takes accountNumber which is validated by the Bank object against the pin
 	public void start(int accountNumber, int pin){
 		
-		Account userAccount = new Account(pin, accountNumber);
+		userAccount = new Account(pin, accountNumber);
 		myBank.validate(userAccount);
 		
 	}
@@ -46,12 +49,44 @@ public class ATM {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		boolean userExit = false;
+		
+		
 		Scanner reader = new Scanner(System.in);
 		
+		while (userExit){
+			System.out.println("Welcome to LLM Banking!");
+			System.out.print("Please enter your account number: ");
+			accountNum = reader.nextInt();
+			
+			System.out.print("Thank you. Now please enter your PIN: ");
+			pin = reader.nextInt();
+			
+			System.out.println("Valdating account...");
+			if (myBank.validate(userAccount) == true){
+				
+				System.out.println("You're in!");
+				
+				while (userExit){
+					
+					System.out.println("Please provide a function: (W)ithdraw, (D)eposit, or (E)xit");
+					String input = reader.next();
+					
+					
+					}
+				}
+				
+				
+				
+				
+				
+			} else { // Not validated
+				System.out.println("Sorry, cannot validate account. Please try again.");
+			}
+			
 		
-		int userAccount = reader.nextInt();
-		
-		
+		}
+		reader.close();
 		
 	}
 
