550059f6a81962ca41806ea6efc5ee4b5f45201d
seanjn57 <aldalali@uwm.edu>
2017-02-14 12:02:14 -0600

    Update Account.java
    account somewhat done

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,15 @@
-
 public class Account {
+public int balance, pin, account;
+	Account(int Balance, int pin, int account){
+		this.account = account;
+		this.balance =Balance;
+		this.pin = pin;
+		
+	}
+	Account( int pin, int account){
+		this.account = account;
+		this.pin = pin;
+		
+	}
 	
 }
