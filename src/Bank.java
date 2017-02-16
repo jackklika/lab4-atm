07ceca64c101e3b20145fa064071f8b8220b5a3f
kmontour <kmontour@CEAS-E270-PC05.ad.uwm.edu>
2017-02-16 11:52:56 -0600

    Added withdraw method

--- src/Bank.java
+++ src/Bank.java
@@ -1,52 +1,52 @@
 import java.util.LinkedList;
 
-public class Bank  {
-LinkedList <Account> Accounts; 
-public Bank(){
-Account a = new Account(80, 6789, 1234); //initial values as described
-Account b = new Account(60, 4321, 6789); //initial values as described 
-}
-public boolean addAccount(int balance, int pin, int account){
-	for(Account i : Accounts){
-		if(i.account == account){
-			return false;
-		}
-	}
-	Accounts.add(new Account(balance, pin, account));
-	return true;
-}
-private boolean validate(Account r){
+public class Bank {
+	LinkedList<Account> Accounts;
 
-	//loop to validate account, changed by kaitlyn
-	for(Account i: Accounts){
-		if (r.account == i.account){
-			return (r.pin == i.pin);
-		}
-			
+	public Bank() {
+		Account a = new Account(80, 6789, 1234); // initial values as described
+		Account b = new Account(60, 4321, 6789); // initial values as described
 	}
-	return false;
-/*
-	 commenting out Johns code
-	if(r.account == a.account){
-		if(r.pin == a.pin){
-			return true;
-		}else{
-			return false;
-		}
-	}else if(r.account == b.account){
-			if(r.pin == b.pin){
-				return true;
-			}else{
+
+	public boolean addAccount(int balance, int pin, int account) {
+		for (Account i : Accounts) {
+			if (i.account == account) {
 				return false;
 			}
-		}else{
-			return false;
 		}
-	 	*/
-	}	
-	
-private boolean withdraw(double amount ){
-	return true;
-}
+		Accounts.add(new Account(balance, pin, account));
+		return true;
+	}
+
+	private boolean validate(Account r) {
+
+		// loop to validate account, changed by kaitlyn
+		for (Account i : Accounts) {
+			if (r.account == i.account) {
+				return (r.pin == i.pin);
+			}
+
+		}
+		return false;
+		/*
+		 * commenting out Johns code if(r.account == a.account){ if(r.pin ==
+		 * a.pin){ return true; }else{ return false; } }else if(r.account ==
+		 * b.account){ if(r.pin == b.pin){ return true; }else{ return false; }
+		 * }else{ return false; }
+		 */
+	}
+
+	private boolean withdraw(double amount, Account r) {
+		for (Account i : Accounts) {
+			if (r.account == i.account) {
+				if (i.balance > amount) {
+					i.balance -= amount;
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
 
 }
