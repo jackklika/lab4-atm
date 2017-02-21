ef4677566a236b828fc33b85808f7315848d6c23
aldal <aldal@10.20.65.194>
2017-02-21 12:29:20 -0600

    changes to customer

--- src/Customer.java
+++ src/Customer.java
@@ -3,12 +3,7 @@ public class Customer {
 	public Card card;
 	public int pin;
 	
-	public Customer(int account){
-		this.card = new Card(account);
-	}
-
-	public Customer(Card tmp) {
-		// TODO Auto-generated constructor stub
-		tmp = c;
+	public Customer(Card userCard){
+		this.card = userCard;
 	}
 }
